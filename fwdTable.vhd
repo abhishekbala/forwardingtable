ab7cf454dfe8c3706ab036ad21a51dc48be86751
Sanmay Jain <sanmay.jain@gmail.com>
2014-10-20 21:58:14 -0400

    CAM Module
    Most syntax errors fixed

--- fwdTable.vhd
+++ fwdTable.vhd
@@ -1,69 +1,73 @@
-library IEEE, STD;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_components.all;
-use IEEE.std_logic_arith.all;
-use IEEE.std_logic_misc.all;
-use IEEE.std_logic_unsigned.all;
+LIBRARY IEEE, STD;
+USE IEEE.std_logic_1164.all;
+--USE IEEE.std_logic_components.all;
+USE IEEE.std_logic_arith.all;
+USE IEEE.std_logic_misc.all;
+USE IEEE.std_logic_unsigned.all;
 
 -- http://www.oocities.org/deepakgeorge2000/cam.htm
 -- Basic set up of a table, configured for 48 bit tag (address), 
 
-entity fwdTable is
-  port     (clk              : in   std_logic;                   --   Clock
-            rst              : in   std_logic;                   --   Reset
-            rd_b             : in   std_logic;                   --   Read
-            wr_b             : in   std_logic;                   --   Write
-            datain           : in   std_logic_vector(2 downto 0);-- data in
+ENTITY fwdTable IS
+  PORT     (clk              : IN   STD_LOGIC;                   --   Clock
+            rst              : IN   STD_LOGIC;                   --   Reset
+            rd_b             : in   STD_LOGIC;                   --   Read
+            wr_b             : in   STD_LOGIC;                   --   Write
+            datain           : in   STD_LOGIC_VECTOR(1 downto 0);-- data in
                                                                  -- during write.
-            tagin            : in   std_logic_vector(47 downto 0);-- Tag Data
-            data_out         : out  std_logic_vector(2 downto 0);-- Data out
-            full             : out  std_logic;                   -- Stack Full  
-            hit              : out  std_logic                    -- Found Match  
+            tagin            : in   STD_LOGIC_VECTOR(47 downto 0);-- Tag Data
+            data_out         : out  STD_LOGIC_VECTOR(1 downto 0);-- Data out
+            full             : out  STD_LOGIC;                   -- Stack Full  
+            hit              : out  STD_LOGIC                    -- Found Match  
            );
-end cam256;
+END fwdTable;
  
  
-architecture cam256_A of cam256 is
-type ram_array is array (0 to 31) of std_logic_vector(2 downto 0);
-signal tag              : ram_array;
-signal data             : ram_array;
-signal count            : integer range 0 to 31;
-begin
+ARCHITECTURE cam256_A OF fwdTable IS
+TYPE tag_array IS ARRAY (0 to 31) of STD_LOGIC_VECTOR(47 downto 0);
+TYPE data_array IS ARRAY (0 to 31) of STD_LOGIC_VECTOR(1 downto 0);
+SIGNAL tag              : tag_array;
+SIGNAL data             : data_array;
+SIGNAL count            : INTEGER RANGE 0 TO 31;
+BEGIN
  
-main : process(clk,rst)
- 
-begin
-  if (rst = '1') then
-    tag<=(others=>'0');
-    data<=(others=>'0');
+PROCESS(clk,rst)
+BEGIN
+	IF (rst = '1') THEN
+    tag<=(OTHERS=>'0');
+	 --tag(0)<= ('000000000000000000000000000000000000000000000000');
+    data<=(OTHERS=>'0');
+	 --data(0)<= ('00');
     count<= 0;
     full <= '0';
  -----------------------------------------------------------------------
  --  Stack Write Operation
  -----------------------------------------------------------------------
-  elsif ( clk'evevt and clk='1') then
-    if (wr_b = '0' and rd_b = '1') then
-      if ( count = 31 ) then
+  ELSIF ( clk'EVENT and clk='1') THEN
+    IF (wr_b = '1' and rd_b = '0') THEN
+      IF ( count = 31 ) THEN
         full <= '1';
-      else
-        tag (conv_integer (count))<= tin;
+		  -- With this code, it does not write if CAM is full.
+		  -- We need to handle that case.
+      ELSE
+        tag (conv_integer (count))<= tagin;
         data(conv_integer (count))<= datain;
         count <= count +1;
-      end if;
+      END IF;
 ---------------------------------------------------------------------------
 --  Stack CAM Read  Operation
 ---------------------------------------------------------------------------
-    elsif (wr_b = '1' and rd_b = '0') then
-      for addr 0 to 31 loop             --   Check for data
-        if ( tagin = tag(conv_integer (addr)) then
+    ELSIF (wr_b = '0' and rd_b = '1') THEN
+      FOR addr IN 0 TO 31 LOOP             --   Check for data
+        IF ( tagin = tag(conv_integer (addr))) THEN
           Hit <= '1';                    --   Found Match
           data_out <= data ( conv_integer (addr));
-        else
+        ELSE
           hit <= '0';                    --   No match found
-        end if;
-      end loop;
-    end if 
-  end if;
-end process; 
+        END IF;
+      END LOOP;
+    END IF; 
+  END IF;
+END PROCESS; 
  
-end cam256_A;
+END cam256_A;
